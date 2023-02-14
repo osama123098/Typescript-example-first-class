b25e3128e2adac9bf7b48535ae29f8003c3cbecf
osama123098 <osamashaikhansari@gmail.com>
2023-02-14 05:47:49 -0500

    q 5

---
+++ q5.js
@@ -0,0 +1,4 @@
+let famous_persion = 'Allama Khadim Hussain Rizvi'
+let message = famous_persion + ': "A war has begun and this is a war of honour of the Prophet ﷺ The one who doesn’t participate in this war, He betrays the Messenger of Allah"'
+
+console.log(message)
