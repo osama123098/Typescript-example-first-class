d0378c748cf49ff4d15c84fba379de2b349fbcb5
osama123098 <osamashaikhansari@gmail.com>
2023-02-14 05:33:23 -0500

    2nd question

--- intro.js
+++ intro.js
@@ -1,2 +1,15 @@
-var temp = 'Osama';
-console.log('Hello my dear' + temp);
+let temp = 'osama khadija love'
+console.log(temp.toUpperCase())
+
+console.log(temp)
+function temporary(str){
+    str = str.toLowerCase().split(' ');
+    for (var i =0 ;i <str.length; i++){
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+        return str.join(' ');
+    }
+
+
+
+console.log(temporary(temp))
--- intro.ts
+++ intro.ts
@@ -1,2 +1,15 @@
-let temp = 'Osama'
-console.log('Hello my dear '+temp)
+let temp = 'osama'
+console.log(temp.toUpperCase())
+
+console.log(temp)
+function temporary(str){
+    str = str.toLowererCase().split(' ');
+    for (var i =0 ;i <str.length; i++){
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+        return str.join(' ');
+    }
+
+
+
+console.log(temporary(temp))
